f6cc0aba457bb617fdfe92ba21490688e7e14eea
Iuliia El Sammak <juliag875@gmail.com>
2022-01-25 17:13:58 -0500

    styled changes

--- script.js
+++ script.js
@@ -5,53 +5,54 @@ function getCities(){
     .then(resp => resp.json())
     .then(data => data.forEach(cityBar)) 
 }
+
+// Function to display leftbar info on main page
 function cityBar(cityObj){
 const cityName = document.createElement('ol');
 cityName.innerText = cityObj.city
 
 const img = document.createElement('img')
-    img.style.width = '80px'
-    img.style.height = '60px'
-    img.src = cityObj.image
+img.style.width = '80px'
+img.style.height = '60px'
+img.src = cityObj.image
 
-    document.querySelector('#cities').append(cityName, img)
+document.querySelector('#cities').append(cityName, img)
     
-
-    img.addEventListener('click', function() {
-       
-        const cityInfo = document.getElementById('cityInfo')
-        const cityName = document.createElement('h2')
-        cityName.innerHTML = cityObj.city
-        const cityPhoto = document.createElement('img')
-        cityPhoto.src = cityObj.image
-        const countryName = document.createElement('h3')
-        countryName.innerHTML = cityObj.country
-        const cityCaption = document.createElement('h4')
-        cityCaption.innerHTML = cityObj.caption
-        const addedBy = document.createElement('p')
-        addedBy.innerHTML = `added by ${cityObj.username}`
-        const likeCount = document.createElement('p')
-        likeCount.innerHTML = `${cityObj.likes} likes`
-
-        const commentContainer = document.createElement('div')
-        commentContainer.innerHTML = "Comments go here"
-        const commentDraftbox = document.createElement('textarea')
-        commentDraftbox.innerHTML = "Type comments here"
-        const commentForm = document.createElement('form')
+img.addEventListener('click', function() {
+const cityInfo = document.getElementById('cityInfo')
+const cityName = document.createElement('h2')
+cityName.innerHTML = cityObj.city
+const cityPhoto = document.createElement('img')
+cityPhoto.src = cityObj.image
+const countryName = document.createElement('h3')
+countryName.innerHTML = cityObj.country
+const cityCaption = document.createElement('h4')
+cityCaption.innerHTML = cityObj.caption
+const addedBy = document.createElement('p')
+addedBy.innerHTML = `added by ${cityObj.username}`
+const likeCount = document.createElement('p')
+likeCount.innerHTML = `${cityObj.likes} likes`
+const commentContainer = document.createElement('div')
+commentContainer.innerHTML = "Comments go here"
+const commentDraftbox = document.createElement('textarea')
+commentDraftbox.innerHTML = "Type comments here"
+const commentForm = document.createElement('form')
 
 
-       cityInfo.append(cityName, cityPhoto, countryName, cityCaption, addedBy, likeCount, commentContainer, commentDraftbox)
-document.querySelector('#new_image').addEventListener('submit', addCity)
-    })
+cityInfo.append(cityName, cityPhoto, countryName, cityCaption, addedBy, likeCount, commentContainer, commentDraftbox)
+})
+
+// Form submit
+const newImage = document.querySelector('#new_image').addEventListener('submit', addCity)
 function addCity(event){
-    event.preventDefault()
-    let cityObj = {
-        country: event.target.country.value,
-        city: event.target.city.value,
-        image: event.target.image.value,
-        caption: event.target.caption.value,
-        likes: 0
-    }
+event.preventDefault()
+let cityObj = {
+    country: event.target.country.value,
+    city: event.target.city.value,
+    image: event.target.image.value,
+    caption: event.target.caption.value,
+    likes: 0
+}
 postCity(cityObj) 
 
 function postCity(cityObj){
